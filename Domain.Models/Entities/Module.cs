7d0710c102709f2f8d64e66f05967769ddfd7e15
Mikael Nordman <mikael.nordman147@gmail.com>
2026-03-18 15:58:02 +0100

    Changed List to ICollection and initilaized them as []

--- Domain.Models/Entities/Module.cs
+++ Domain.Models/Entities/Module.cs
@@ -3,9 +3,9 @@
 public class Module
 {
     public int Id { get; set; }
-    public string Name { get; set; } = string.Empty;
-    public string Description { get; set; } = string.Empty;
+    public string Name { get; set; }
+    public string Description { get; set; }
     public DateTime StartDate { get; set; }
     public DateTime EndDate { get; set; }
-    public List<ModuleTeacher> Teachers { get; set; } = new List<ModuleTeacher>();
+    public ICollection<ModuleTeacher> Teachers { get; set; } = [];
 }
