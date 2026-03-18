9dbc9782ff2f1274c3b3194caecd36489d0d6c43
Mikael Nordman <mikael.nordman147@gmail.com>
2026-03-18 15:52:54 +0100

    Added entities

---
+++ Domain.Models/Entities/ActivityType.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain.Models.Entities;
+
+public class ActivityType
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public bool TimeExclusive { get; set; }
+}
---
+++ Domain.Models/Entities/ModuleTeacher.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Models.Entities;
+
+public class ModuleTeacher
+{
+    public int Id { get; set; }
+    public int ModuleId { get; set; }
+    public Module Module { get; set; }
+    public string TeacherId { get; set; }
+    public ApplicationUser Teacher { get; set; }
+}
