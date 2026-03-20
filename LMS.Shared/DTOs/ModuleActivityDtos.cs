6ab8e70b5d6d4d186a983d663f98aefbaf2ffe3e
Mikael Nordman <mikael.nordman147@gmail.com>
2026-03-20 10:45:01 +0100

    Created Dtos

---
+++ LMS.Shared/DTOs/ModuleActivityDtos.cs
@@ -0,0 +1,20 @@
+﻿namespace LMS.Shared.DTOs
+{
+    public class ActivityUpsertDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+
+    public class ActivityReadDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int ModuleActivityTypeId { get; set; }
+    }
+}
