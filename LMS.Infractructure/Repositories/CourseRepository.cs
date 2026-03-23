ba61acd14566ab68147307d9e4bb349dd816ba4f
Christopher Clemons (Windows) <chris.clemons85@gmail.com>
2026-03-23 10:00:49 +0100

    add initial CourseRepository implementation

---
+++ LMS.Infractructure/Repositories/CourseRepository.cs
@@ -0,0 +1,34 @@
+﻿using Domain.Contracts.Repositories;
+using Domain.Models.Entities;
+using System.Linq.Expressions;
+
+namespace LMS.Infractructure.Repositories
+{
+    public class CourseRepository : ICourseRepository
+    {
+        public void Create(Course entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Course entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<Course> FindAll(bool trackChanges = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<Course> FindByCondition(Expression<Func<Course, bool>> expression, bool trackChanges = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Course entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
