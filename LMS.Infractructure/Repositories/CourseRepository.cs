aee22ce6c3a808e89535c2b31b8bddc7398919a6
Christopher Clemons (Windows) <chris.clemons85@gmail.com>
2026-03-23 10:08:42 +0100

    fix: have CorseRepository inherit from RepositoryBase

--- LMS.Infractructure/Repositories/CourseRepository.cs
+++ LMS.Infractructure/Repositories/CourseRepository.cs
@@ -1,34 +1,10 @@
 ﻿using Domain.Contracts.Repositories;
 using Domain.Models.Entities;
-using System.Linq.Expressions;
+using LMS.Infractructure.Data;
 
 namespace LMS.Infractructure.Repositories
 {
-    public class CourseRepository : ICourseRepository
+    public class CourseRepository(ApplicationDbContext context) : RepositoryBase<Course>(context), ICourseRepository
     {
-        public void Create(Course entity)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Delete(Course entity)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IQueryable<Course> FindAll(bool trackChanges = false)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IQueryable<Course> FindByCondition(Expression<Func<Course, bool>> expression, bool trackChanges = false)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Update(Course entity)
-        {
-            throw new NotImplementedException();
-        }
     }
 }
