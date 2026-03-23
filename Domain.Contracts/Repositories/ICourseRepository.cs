c7f2cbba35fbd565848261b2b1cdf0a83a0bf392
Christopher Clemons (Windows) <chris.clemons85@gmail.com>
2026-03-23 16:23:45 +0100

    add GetCourseDetailsByConditionAsync method

--- Domain.Contracts/Repositories/ICourseRepository.cs
+++ Domain.Contracts/Repositories/ICourseRepository.cs
@@ -1,8 +1,12 @@
 ﻿using Domain.Models.Entities;
+using System.Linq.Expressions;
 
 namespace Domain.Contracts.Repositories
 {
     public interface ICourseRepository : IInternalRepositoryBase<Course>, IRepositoryBase<Course>
     {
+        Task<bool> CourseExistsAsync(int courseId);
+
+        Task<Course?> GetCourseDetailsByConditionAsync(Expression<Func<Course, bool>> expression, bool trackChanges = false);
     }
 }
--- LMS.Infractructure/Repositories/CourseRepository.cs
+++ LMS.Infractructure/Repositories/CourseRepository.cs
@@ -1,10 +1,27 @@
 ﻿using Domain.Contracts.Repositories;
 using Domain.Models.Entities;
 using LMS.Infractructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
 
 namespace LMS.Infractructure.Repositories
 {
     public class CourseRepository(ApplicationDbContext context) : RepositoryBase<Course>(context), ICourseRepository
     {
+        ApplicationDbContext context = context;
+        public async Task<bool> CourseExistsAsync(int courseId)
+        {
+            return await context.Courses.FindAsync(courseId) is not null;
+        }
+
+        public async Task<Course?> GetCourseDetailsByConditionAsync(Expression<Func<Course, bool>> expression, bool trackChanges = false)
+        {
+            var baseQuery = !trackChanges ? context.Courses.AsNoTracking() : context.Courses;
+
+            return await FindByCondition(expression, trackChanges)
+                .Include(c => c.Participants)
+                .Include(c => c.Modules)
+                .SingleAsync();
+        }
     }
 }
