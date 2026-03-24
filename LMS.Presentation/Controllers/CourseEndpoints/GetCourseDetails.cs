db37b768ec95ab700b8eb3f378ed3c59a7ddef1d
Christopher Clemons (Windows) <chris.clemons85@gmail.com>
2026-03-24 14:42:37 +0100

    add Authorize attribute

--- LMS.Presentation/Controllers/CourseEndpoints/GetCourseDetails.cs
+++ LMS.Presentation/Controllers/CourseEndpoints/GetCourseDetails.cs
@@ -1,15 +1,16 @@
 ﻿using LMS.Shared.DTOs;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Service.Contracts;
 
 namespace LMS.Presentation.Controllers.CourseEndpoints
 {
+    [Authorize]
     [Route("api/courses")]
     [ApiController]
     public partial class CourseController(IServiceManager serviceManager) : ControllerBase
     {
         private readonly ICourseService courseService = serviceManager.CourseService;
-    
 
         [HttpGet("{id:int}")]
         public async Task<IActionResult> GetCourseDetails([FromRoute] int id)
