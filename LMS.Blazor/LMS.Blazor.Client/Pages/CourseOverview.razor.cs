72218694c82a4d5f17bb77e4aec988dc67c88738
Christopher Clemons (Windows) <chris.clemons85@gmail.com>
2026-03-24 13:20:14 +0100

    load demo course title on CourseOverview page

--- LMS.Blazor/LMS.Blazor.Client/Pages/CourseOverview.razor.cs
+++ LMS.Blazor/LMS.Blazor.Client/Pages/CourseOverview.razor.cs
@@ -1,10 +1,24 @@
 using System;
+using LMS.Blazor.Client.Services;
+using LMS.Shared.DTOs;
 using Microsoft.AspNetCore.Components;
 
 namespace LMS.Blazor.Client.Pages;
 
 public partial class CourseOverview
 {
+    [Inject]
+    private IApiService ApiService { get; set; } = default!;
+
     [Parameter]
     public string CourseId { get; set; } = string.Empty;
+
+    private int? courseId { get; set; } = null;
+
+    private CourseDetailsDto? CourseDetails { get; set; } = null;
+
+    protected override async Task OnInitializedAsync()
+    {
+        CourseDetails = await ApiService.GetAsync<CourseDetailsDto>("/api/courses/4", new CancellationToken());
+    }
 }
