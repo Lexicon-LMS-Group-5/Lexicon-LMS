7980aec9458eeedf50697a5f5cd58758dfbb0b37
Christopher Clemons (Windows) <chris.clemons85@gmail.com>
2026-03-24 10:32:29 +0100

    add CourseService with GetCourseDetailsAsync method

---
+++ LMS.Services/CourseService.cs
@@ -0,0 +1,16 @@
+﻿using LMS.Shared.DTOs;
+using Service.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LMS.Services
+{
+    public class CourseService : ICourseService
+    {
+        public Task<CourseDetailsDto> GetCourseDetailsAsync(CourseDetailsQueryDto query)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- LMS.Shared/DTOs/CourseDtos.cs
+++ LMS.Shared/DTOs/CourseDtos.cs
@@ -2,8 +2,8 @@
 {
     public class CourseUpsertDto
     {
-        public string Name { get; set; }
-        public string Description { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
     }
@@ -11,9 +11,32 @@
     public class CourseReadDto
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
+    }
+
+    public record CourseDetailsQueryDto(int CourseId);
+
+    public class CourseDetailsDto : CourseReadDto
+    {
+        IEnumerable<CourseParticipantsWithRoleInfoDto> Participants { get; set; } = [];
+        IEnumerable<CourseModuleListItemDto> Modules { get; set; } = [];
+    }
+
+    public class CourseParticipantsWithRoleInfoDto
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Role { get; set; } = string.Empty;
+    }
+
+    public class CourseModuleListItemDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
     }
 }
