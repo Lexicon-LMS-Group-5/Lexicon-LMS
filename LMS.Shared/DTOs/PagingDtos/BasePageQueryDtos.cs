a6d2ae53e077a0f283ef0630591f19344c9187c5
Christopher Clemons (Windows) <chris.clemons85@gmail.com>
2026-03-26 10:49:19 +0100

    add BasePageQueryDtos

---
+++ LMS.Shared/DTOs/PagingDtos/BasePageQueryDtos.cs
@@ -0,0 +1,25 @@
+﻿namespace LMS.Shared.DTOs.PagingDtos;
+
+public class BasePageQueryDto
+{
+}
+
+public class BasePagedResultDto<TItem>
+{
+    public IReadOnlyList<TItem> Items { get; }
+}
+
+public class PagedResultMetadataDto
+{
+    public int Page { get; set; }
+    public int Size { get; set; }
+    public int TotalPages { get; set; }
+    public int TotalCount { get; set; }
+    public SortOrder Order { get; set; }
+}
+
+public enum SortOrder
+{
+    Descending,
+    Ascending,
+}
