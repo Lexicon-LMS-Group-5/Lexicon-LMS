b5a4befd358a6219dfbb9fd36c18cd86b7dbe399
Christopher Clemons (Windows) <chris.clemons85@gmail.com>
2026-03-26 16:44:25 +0100

    use base properties from CourseReadDto in CourseListItemDto

--- LMS.Shared/DTOs/CourseDtos/CourseQueryDtos.cs
+++ LMS.Shared/DTOs/CourseDtos/CourseQueryDtos.cs
@@ -12,11 +12,7 @@ public class CoursesQueryResultDto : BasePagedResultDto<CourseListItemDto>
 
 }
 
-public class CourseListItemDto
+public class CourseListItemDto : CourseReadDto
 {
-    public int Id { get; set; }
-    public string Name { get; set; } = string.Empty;
-    public string Description { get; set; } = string.Empty;
-    public DateTime StartDate { get; set; }
-    public DateTime EndDate { get; set; }
+
 }
