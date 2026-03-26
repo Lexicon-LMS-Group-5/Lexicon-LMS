d1c1183e54c5670f6507c88383b0287bfd24e09a
Christopher Clemons (Windows) <chris.clemons85@gmail.com>
2026-03-26 09:00:49 +0100

    move GetCourseDetails endpoint to CourseController

---
+++ LMS.Presentation/Controllers/CourseController.cs
@@ -0,0 +1,22 @@
+﻿using LMS.Shared.DTOs;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Service.Contracts;
+
+namespace LMS.Presentation.Controllers;
+
+[Authorize]
+[Route("api/courses")]
+[ApiController]
+public class CourseController(IServiceManager serviceManager) : ControllerBase
+{
+    private readonly ICourseService courseService = serviceManager.CourseService;
+
+    [HttpGet("{id:int}")]
+    public async Task<IActionResult> GetCourseDetails([FromRoute] int id)
+    {
+        var result = await courseService.GetCourseDetailsAsync(new CourseDetailsQueryDto(id));
+
+        return Ok(result);
+    }
+}
