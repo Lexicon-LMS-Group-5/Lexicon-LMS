9e1cd9e563b2e51a61b7c084aca7295e1244cf4f
Christopher Clemons (Windows) <chris.clemons85@gmail.com>
2026-03-26 16:38:05 +0100

    show list of Courses for Teachers on Home page

--- LMS.Blazor/LMS.Blazor.Client/Pages/Home.razor.cs
+++ LMS.Blazor/LMS.Blazor.Client/Pages/Home.razor.cs
@@ -1,4 +1,7 @@
 ﻿
+using LMS.Blazor.Client.Models;
+using LMS.Blazor.Client.Services;
+using LMS.Shared.DTOs.CourseDtos;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Authorization;
 
@@ -7,11 +10,15 @@ namespace LMS.Blazor.Client.Pages;
 public partial class Home
 {
     [CascadingParameter]
-    private Task<AuthenticationState> authenticationStateTask { get; set; }
+    private Task<AuthenticationState> authenticationStateTask { get; set; } = default!;
 
     [Inject]
-    public NavigationManager Navigation { get; set; }
+    private IApiService ApiService { get; set; } = default!;
 
+    [Inject]
+    public NavigationManager Navigation { get; set; } = default!;
+
+    private PageState<CoursesQueryResultDto> PageState { get; set; } = new(isLoading: true, data: null);
     protected override async Task OnInitializedAsync()
     {
         var authenticationState = await authenticationStateTask;
@@ -19,6 +26,19 @@ public partial class Home
         if (!authenticationState.User.IsInRole("Teacher"))
         {
             Navigation.NavigateTo("my-course");
+        } else
+        {
+            PageState = new(isLoading: true, data: null);
+
+            try
+            {
+                var result = await ApiService.GetAsync<CoursesQueryResultDto>("api/courses")
+                    ?? throw new Exception("Could not fetch Courses");
+                PageState = new(isLoading: false, data: result);
+            }
+            catch (Exception ex) {
+                PageState = new(isLoading: false, data: null, error: ex.Message);
+            } 
         }
     }
 }
