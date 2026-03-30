02c02fa63b6ee862d62c1df619453b394b4ed005
mps1967 <mihaips@gmail.com>
2026-03-30 08:48:13 +0200

    some ModuleController work

---
+++ LMS.Presentation/Controllers/ModuleController.cs
@@ -0,0 +1,31 @@
+﻿using LMS.Shared.DTOs;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Service.Contracts;
+
+namespace LMS.Presentation.Controllers
+{
+    [Authorize]
+    [Route("api/modules")]
+    [ApiController]
+    [Produces("application/json")]
+
+    public class ModuleController : ControllerBase
+    {
+        private readonly IServiceManager serviceManager;
+
+        public ModuleController(IServiceManager serviceManager)
+        {
+            this.serviceManager = serviceManager;
+        }
+        [HttpGet("{cid:int}")]
+        [ProducesResponseType<List<ModuleReadDto>>(StatusCodes.Status200OK)]
+        public async Task<IActionResult> GetModulesByCourseId([FromRoute] int cid)
+        {
+            var result = await serviceManager.ModuleService.GetModulesByCourseIdAsync(new ModulesByCourseIdQueryDto(cid));
+            return Ok(result);
+
+
+        }
+}
