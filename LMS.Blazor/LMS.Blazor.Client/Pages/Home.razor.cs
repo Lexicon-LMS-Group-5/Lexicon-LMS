9ef6f821f0b67f2204bf02b5a4a659c4f06ad47a
Christopher Clemons (Windows) <chris.clemons85@gmail.com>
2026-04-01 19:17:32 +0200

    update rendered course lists on new course creation

--- LMS.Blazor/LMS.Blazor.Client/Pages/Home.razor.cs
+++ LMS.Blazor/LMS.Blazor.Client/Pages/Home.razor.cs
@@ -18,7 +18,12 @@ public partial class Home
     [Inject]
     public NavigationManager Navigation { get; set; } = default!;
 
+    private string? Notification { get; set; } = null;
+
     private PageState<CoursesQueryResultDto> PageState { get; set; } = new(isLoading: true, data: null);
+
+    private IReadOnlyList<CourseListItemDto>? AllCourses { get; set; } = null;
+    private IReadOnlyList<CourseListItemDto>? ActiveCourses { get; set; } = null;
     protected override async Task OnInitializedAsync()
     {
         var authenticationState = await authenticationStateTask;
@@ -35,10 +40,23 @@ public partial class Home
                 var result = await ApiService.GetAsync<CoursesQueryResultDto>("api/courses")
                     ?? throw new Exception("Could not fetch Courses");
                 PageState = new(isLoading: false, data: result);
+
+
+                AllCourses = result.Items.ToList();
+                ActiveCourses = result.Items.Where(c => c.EndDate > DateTime.Now).ToList();
             }
             catch (Exception ex) {
                 PageState = new(isLoading: false, data: null, error: ex.Message);
             } 
         }
     }
+
+    private async Task UpdateCourseListsAsync(Func<IReadOnlyList<CourseListItemDto>, IReadOnlyList<CourseListItemDto>> updateList)
+    {
+        AllCourses = updateList(AllCourses ?? []);
+        ActiveCourses = updateList(ActiveCourses ?? [])
+            .Where(c => c.EndDate > DateTime.Now).ToList();
+    }
+
+    private EventCallback<CreateCourseResultDto> OnCourseListUpdated { get; set; }
 }
