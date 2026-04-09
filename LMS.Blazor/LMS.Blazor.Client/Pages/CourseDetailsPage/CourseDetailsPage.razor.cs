934964222bc1cdb2898ef394a0f1890dfb3a9deb
Christopher Clemons (Windows) <chris.clemons85@gmail.com>
2026-04-09 19:35:56 +0200

    improve loading state

---
+++ LMS.Blazor/LMS.Blazor.Client/Pages/CourseDetailsPage/CourseDetailsPage.razor.cs
@@ -0,0 +1,51 @@
+﻿using LMS.Blazor.Client.Services;
+using LMS.Shared.DTOs;
+using Microsoft.AspNetCore.Components;
+
+namespace LMS.Blazor.Client.Pages.CourseDetailsPage;
+
+public partial class CourseDetailsPage
+{
+    [Inject]
+    private IApiService ApiService { get; set; } = default!;
+
+    [Inject]
+    private NavigationManager Navigation { get; set; } = default!;
+
+    [Parameter]
+    public string? CourseId { get; set; }
+
+    [SupplyParameterFromQuery(Name = "ref")]
+    public string? Referrer { get; set; }
+
+    [SupplyParameterFromQuery(Name = "refTab")]
+    public string? ReferrerTab { get; set; }
+
+    private bool IsLoading { get; set; } = true;
+    private string? Error { get; set; } = null;
+    private CourseDetailsDto? CourseDetails { get; set; }
+
+    protected override async Task OnInitializedAsync()
+    {
+        IsLoading = true;
+
+        if (!int.TryParse(CourseId, out int Id))
+            Navigation.NotFound();
+
+        try
+        {
+            var result = CourseDetails ?? await ApiService.GetAsync<CourseDetailsDto>($"api/courses/{Id}")
+                ?? throw new Exception("Could not fetch course details");
+
+            CourseDetails = result;
+        }
+        catch (Exception ex)
+        {
+            Error = ex.Message;
+        }
+        finally
+        {
+            IsLoading = false;
+        }
+    }
+}
