3c206526a042c92875e26d8b0785115806725f43
Mikael Nordman <mikael.nordman147@gmail.com>
2026-04-10 11:53:42 +0200

    Formating improvments in IServiceManager

--- Service.Contracts/IServiceManager.cs
+++ Service.Contracts/IServiceManager.cs
@@ -2,11 +2,9 @@
 public interface IServiceManager
 {
     IAuthService AuthService { get; }
-
     ICourseService CourseService { get; }
     IModuleService ModuleService { get; }
     IActivityService ActivityService { get; }
     IActivityTypeService ActivityTypeService { get; }
-
     IUserService UserService { get; }
 }
